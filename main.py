891796ebe853e80a2e77c06dc549fe92ae8b74d6
Huy Dao Ngoc <huydao2802@gmail.com>
2024-09-15 23:13:53 +0700

    [add][huydn]: add folder structure

---
+++ main.py
@@ -0,0 +1,20 @@
+import http
+import json
+
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
+
+app = FastAPI()
+app.title = "Name of Project"
+app.version = "0.0.1"
+
+# Create a GET method that responds with HTML code
+@app.get('/', tags = ['home'])
+def message():
+    return HTMLResponse('<h1>Welcome to Name of Project API Services</h1>')
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
